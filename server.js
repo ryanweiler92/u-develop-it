506a35b8e46fbec61ff29ee8a061bcd1e6043b9b
ryanweiler92 <ryanweiler92@gmail.com>
2022-02-02 09:58:47 -0500

    joined tables

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const db = mysql.createConnection(
 
 //Get All candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
 db.query(sql, (err, rows) => {
     if (err) {
@@ -41,8 +45,13 @@ db.query(sql, (err, rows) => {
 
 //GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-const sql = `SELECT * FROM candidates WHERE id = ?`;
-const params = [req.params.id];
+    const sql = `SELECT candidates.*, parties.name 
+            AS party_name 
+            FROM candidates 
+            LEFT JOIN parties 
+            ON candidates.party_id = parties.id 
+            WHERE candidates.id = ?`;
+    const params = [req.params.id];
 
 db.query(sql, params, (err, row) => {
     if (err) {
